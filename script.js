6232c2fe45076d7c201ba463a675e2856edf557b
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-11 14:17:18 +0530

    Add IDs to buttons and store the values in variables

--- script.js
+++ script.js
@@ -49,3 +49,15 @@ function appendChar() {
 function calculate() {
 
 }
+
+//  Store the the number of the buttons in variables
+const button0 = document.querySelector('#button0');
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const button4 = document.querySelector('#button4');
+const button5 = document.querySelector('#button5');
+const button6 = document.querySelector('#button6');
+const button7 = document.querySelector('#button7');
+const button8 = document.querySelector('#button8');
+const button9 = document.querySelector('#button9');
