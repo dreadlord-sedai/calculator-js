42de6af57b7f99fd20a18a46112f08ad69b5c264
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-11 13:50:54 +0530

    Add basic arithmetic functions in script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let a;
+let operator;
+let b;
+
+function operate(a,operator,b) {
+  switch (operator) {
+    case '+':
+      return add(a,b);
+    case '-':
+      return subtract(a,b);
+    case '*':
+      return multiply(a,b);
+    case '/':
+      return divide(a,b);
+  }
+}
+
